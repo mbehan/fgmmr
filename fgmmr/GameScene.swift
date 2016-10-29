7026b82bbe5676f1403d690ab39e0e6b8c9e6dbb
Michael Behan <michael@marinosoftware.com>
2016-10-29 01:40:29 +0100

    Work on initial physics simulation

--- fgmmr/GameScene.swift
+++ fgmmr/GameScene.swift
@@ -7,66 +7,122 @@
 //
 
 import SpriteKit
-import GameplayKit
+
+let gravitationalConstant : CGFloat = 0.1
+
+func sub(a: CGPoint, b: CGPoint) -> CGPoint {
+    return CGPoint(x: a.x - b.x ,y: a.y - b.y)
+}
+
+func add(a: CGPoint, b: CGPoint) -> CGPoint {
+    return CGPoint(x: a.x + b.x ,y: a.y + b.y)
+}
+
+func mult(a: CGPoint, b: CGFloat) -> CGPoint {
+    return CGPoint(x: a.x * b ,y: a.y * b)
+}
+
+func length(a: CGPoint) -> CGFloat {
+    return sqrt(CGFloat(a.x * a.x) + CGFloat(a.y * a.y))
+}
+
+// Makes a vector have a length of 1
+func normalize(a:CGPoint)->CGPoint{
+    let l = length(a: a)
+    return CGPoint(x: a.x / l, y: a.y / l)
+}
+
+func distance(p1:CGPoint, p2:CGPoint) -> CGFloat {
+    return CGFloat(hypotf(Float(p1.x - p2.x), Float(p1.y - p2.y)))
+}
+
+class GravitationalSystem {
+    var bodies = [SKShapeNode]()
+    
+    func update(){
+        for thisBody in bodies {
+            
+            for otherBody in bodies {
+             
+               // let forceBetweenBodies =
+            }
+        }
+    }
+}
+
+struct Planet : Equatable {
+    
+    let node : SKNode
+    var position : CGPoint {
+        didSet {
+            node.position = self.position
+        }
+    }
+    
+    let mass : CGFloat
+    
+    init(mass m : CGFloat? = nil) {
+        
+        if let m = m {
+            mass = m
+        } else {
+            mass = CGFloat(arc4random_uniform(20)) + 1.0
+        }
+        let planet = SKShapeNode.init(circleOfRadius: mass/10.0)
+        let body = SKPhysicsBody(circleOfRadius: mass)
+        
+        body.affectedByGravity = false
+        body.allowsRotation = true
+        
+        planet.physicsBody = body
+        body.collisionBitMask = 0
+        
+        node = planet
+        position = node.position
+        
+        
+    }
+    
+    static func ==(lhs:Planet, rhs:Planet) -> Bool { // Implement Equatable
+        return lhs.node.position == rhs.node.position && lhs.mass == rhs.mass
+    }
+}
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var planets = [Planet]()
+    
+    var added2 = false
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        let planet = Planet()
+        planets.append(planet)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        self.addChild(planet.node)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+        var planet = Planet()
+        planet.position = pos
+        
+        planets.append(planet)
+        self.addChild(planet.node)
+        
+        added2 = true
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
@@ -85,5 +141,87 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        
+        if added2 {
+            
+           
+            
+            func applyGravitationalForce(planet1:Planet, planet2:Planet) {
+                let offset = sub(a: planet1.node.position, b: planet2.node.position)
+                let direction = normalize(a: offset)
+                
+                let d = distance(p1: planet1.node.position, p2: planet2.node.position)
+                let force = gravitationalConstant * ((planet1.mass * planet2.mass)/d)
+                
+                let amount = mult(a: direction, b: force)
+                
+                let forceVector = CGVector(dx: amount.x, dy: amount.y)
+                
+                //print(forceVector)
+                
+                planet2.node.physicsBody?.applyForce(forceVector)
+            }
+            
+            var planetsToKill = [Planet]()
+            var planetsToAdd = [Planet]()
+            
+            for planet1 in planets {
+                for planet2 in planets {
+                    if planet1.position != planet2.position {
+                        
+                        if distance(p1: planet1.node.position, p2: planet2.node.position) < 2 {
+                            planetsToKill.append(planet1)
+                            
+                            var v1 = CGFloat((4.0 / 3.0) * M_PI)
+                            v1 = v1 * (planet1.mass * planet1.mass * planet1.mass)
+                            
+                            var v2 = CGFloat((4.0 / 3.0) * M_PI)
+                            v2 = v2 * (planet2.mass * planet2.mass * planet2.mass)
+                            
+                            let v3 = v1 + v2
+                            
+                            var newR = (v3 / CGFloat(M_PI)) * CGFloat(3.0 / 4.0)
+                            newR = pow(newR, 1.0/3.0)
+                            
+                            let reducingFactor = CGFloat(0.2)
+                            
+                            let newVelocity = CGVector(dx: reducingFactor * planet1.node.physicsBody!.velocity.dx + reducingFactor * planet2.node.physicsBody!.velocity.dx, dy: reducingFactor * planet1.node.physicsBody!.velocity.dy + reducingFactor * planet2.node.physicsBody!.velocity.dy)
+                            
+                            var newPlanet = Planet(mass: newR)
+                            newPlanet.position = CGPoint(x: (planet1.node.position.x + planet2.node.position.x) / 2.0, y:(planet1.node.position.y + planet2.node.position.y) / 2.0)
+                            
+                            newPlanet.node.physicsBody?.velocity = newVelocity
+                            
+                            if !planetsToAdd.contains(newPlanet) {
+                                planetsToAdd.append(newPlanet)
+                            }
+                            
+                            
+                        } else {
+                        
+                            applyGravitationalForce(planet1: planet1, planet2: planet2)
+                        }
+                    }
+                }
+            }
+            
+            if planetsToKill.count > 0 {
+            
+                planets = planets.filter {
+                    if planetsToKill.contains($0) {
+                        $0.node.removeFromParent()
+                        return false
+                    }
+                    return true
+                }
+                
+                
+                for planet in planetsToAdd {
+                    self.addChild(planet.node)
+                    planets.append(planet)
+                }
+                
+            }
+        }
     }
 }
